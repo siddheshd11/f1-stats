9b9468c417bb6a0ba9fa6911a8d54b59349414d8
Siddhesh <siddheshd11@gmail.com>
2025-08-04 14:22:05 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,28 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  // Build configuration
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false, // Set to true if you want source maps
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: false,    // Remove console.log
+        drop_debugger: true,   // Remove debugger statements
+        pure_funcs: ['console.log', 'console.info', 'console.debug'] // Remove specific console methods
+      }
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      }
+    }
+  },
+  base: './', // Use relative paths
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
